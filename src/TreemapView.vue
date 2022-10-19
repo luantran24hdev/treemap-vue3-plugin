4568bf99d0df63a806676a8a6fea29015c2a1e62
elcoruco <el.coruco@gmail.com>
2022-10-19 16:06:05 -0500

    some changes

--- src/TreemapView.vue
+++ src/TreemapView.vue
@@ -56,7 +56,14 @@ const items  = computed( () => !nodes.value ? null : tree.value(nodes.value.sum(
       :class="`gf_${chartName}_svg`">
 
       <g v-if="items">
-        <rect v-for="(item, i) of items" :key="`rect-${i}`"></rect>
+        <rect v-for="(item, i) of items" 
+          :x="item.x0"
+          :y="item.y0"
+          :fill="item.color" 
+          :width="item.x1 - item.x0" 
+          :height="item.y1 - item.y0"
+        :key="`rect-${i}`">
+        </rect>
       </g>
     </svg>
   </div>
